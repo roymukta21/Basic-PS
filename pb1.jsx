2658de01da6ba5bddef026712942bfd6254897d7
MUKTA ROY <muktaroy520@gmail.com>
2026-06-08 16:15:26 +0600

    Problem 3

--- pb1.jsx
+++ pb1.jsx
@@ -27,3 +27,10 @@
 
 // console.log(isEven(4)); // true
 // console.log(isEven(7)); // false
+
+
+// Problem 3: Find the Largest of Three Numbers  [Easy]
+// Description: Write a function largest(a, b, c) that returns the largest of three numbers.
+// Example:
+// Input: 3, 7, 5  → Output: 7
+// Hint: Use Math.max() or if-else conditions.
