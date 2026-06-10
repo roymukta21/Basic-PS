ec927611bcc64bc63f45d9b68f64a38237e4d070
MUKTA ROY <muktaroy520@gmail.com>
2026-06-10 19:23:28 +0600

    Problem 12

--- pb3.jsx
+++ pb3.jsx
@@ -23,7 +23,19 @@ console.log(sumArray([1, 2, 3, 4, 5]));
 
  
  
- 
+ const findMax = (arr) => {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+};
+
+console.log(findMax([3, 1, 7, 2, 9]));
  
  
  
