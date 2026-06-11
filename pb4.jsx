2fcf8f9108af394e52c36f5000221fa02b17fac9
MUKTA ROY <muktaroy520@gmail.com>
2026-06-11 16:11:26 +0600

    pb18

--- pb4.jsx
+++ pb4.jsx
@@ -35,8 +35,22 @@ console.log(mergeObjects({ a: 1 }, { b: 2 }));
 // Input: 15Output: 1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz
 // Hint: Check divisibility with the % operator in the right order.
 
- 
- 
+ const fizzBuzz = (n) => {
+    let result =[];
+    for (let i = 1; i <= n; i++){
+        if (i % 15 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+    }
+     return result;
+ }
+
  
  
  
