d57fda8155d03ab0967b84c19b9c92cfc483799f
MUKTA ROY <muktaroy520@gmail.com>
2026-06-14 16:13:23 +0600

    solve the pb3

--- pb6.jsx
+++ pb6.jsx
@@ -39,9 +39,13 @@
 // delay(1000).then(() => { console.log('1 sec'); return delay(2000); }).then(() => console.log('3 sec total'));
 // Hint: Use new Promise with setTimeout inside.
 
+function delay(ms){
+    return new Promise((resolve) =>{
+        setTimeout(resolve, ms);
+    })
+}
 
-
- 
+ delay(1000).then(() => { console.log('1 sec'); return delay(2000); }).then(() => console.log('3 sec total'));
  
  
  
