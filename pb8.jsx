8d60c13fdae6a996896a63ecf630854280e3119b
MUKTA ROY <muktaroy520@gmail.com>
2026-06-17 22:53:49 +0600

    pb9

--- pb8.jsx
+++ pb8.jsx
@@ -71,35 +71,35 @@
 // Hint: Use recursion; build the key by joining parent keys with dots.
 
  
- function flattenObject(obj, parentKey = "", result = {}) {
-  for (let key in obj) {
-    const newKey = parentKey
-      ? `${parentKey}.${key}`
-      : key;
+//  function flattenObject(obj, parentKey = "", result = {}) {
+//   for (let key in obj) {
+//     const newKey = parentKey
+//       ? `${parentKey}.${key}`
+//       : key;
 
-    if (
-      typeof obj[key] === "object" &&
-      obj[key] !== null &&
-      !Array.isArray(obj[key])
-    ) {
-      flattenObject(obj[key], newKey, result);
-    } else {
-      result[newKey] = obj[key];
-    }
-  }
+//     if (
+//       typeof obj[key] === "object" &&
+//       obj[key] !== null &&
+//       !Array.isArray(obj[key])
+//     ) {
+//       flattenObject(obj[key], newKey, result);
+//     } else {
+//       result[newKey] = obj[key];
+//     }
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-const obj = {
-  a: {
-    b: {
-      c: 1
-    }
-  }
-};
+// const obj = {
+//   a: {
+//     b: {
+//       c: 1
+//     }
+//   }
+// };
 
-console.log(flattenObject(obj));
+// console.log(flattenObject(obj));
 
  
  
