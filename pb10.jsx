eab4a81912d7ebc437a55c1340bd0727667eb90f
MUKTA ROY <muktaroy520@gmail.com>
2026-06-18 17:08:19 +0600

    pb49

--- pb10.jsx
+++ pb10.jsx
@@ -59,7 +59,36 @@ console.log(rotateArray([1,2,3,4,5], 2))
 // Hint: Map each symbol to its value; if a smaller value comes before a larger one, subtract it.
 
 
+function romanToInt(s) {
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
 
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let current = map[s[i]];
+    let next = map[s[i + 1]];
+
+    if (next > current) {
+      result -= current;
+    } else {
+      result += current;
+    }
+  }
+
+  return result;
+}
+
+console.log(romanToInt("III"));   
+console.log(romanToInt("IX"));   
+console.log(romanToInt("LVIII")); 
 
 
 
